79af2d1fea550d1f07879761a9bd979c17ccbb59
v.dominic <v.dominic@featuremind.com>
2025-02-08 10:25:57 +0100

    Added companies worked

--- src/components/partial/home/Work.tsx
+++ src/components/partial/home/Work.tsx
@@ -3,16 +3,12 @@ import { Props } from "../../../theme/styling/GlobalStyle";
 import Image from "next/image";
 import Fade from 'react-reveal/Fade';
 import { Container } from "../../Container";
-import nova from '../../../../public/assets/images/logo/nova.png';
-import bict from '../../../../public/assets/images/logo/bict.png';
-import cousant from '../../../../public/assets/images/logo/cousent.png';
-import devAmplify from '../../../../public/assets/images/logo/dev_amplify.png';
-import oakSoft from '../../../../public/assets/images/logo/oak_soft.png';
-import hoh from '../../../../public/assets/images/logo/hoh.png';
-import exai from '../../../../public/assets/images/logo/exai.svg';
+import fm from '../../../../public/assets/images/logo/featuremind.png'
+import oyiche from '../../../../public/assets/images/logo/oyiche.png'
+import groovinx from '../../../../public/assets/images/logo/groovinx.jpeg'
 
-type d = {icon: string, title: string}
-const skills : d[] = [
+type d = { icon: string, title: string }
+const skills: d[] = [
     {
         icon: 'bx bx-mobile-alt',
         title: 'Mobile App <br/> Development'
@@ -29,7 +25,7 @@ const skills : d[] = [
 
 const Wrapper = styled.section`
     padding-block: 6em;
-    background-color: ${({theme}: Props) => theme.secondaryBg};
+    background-color: ${({ theme }: Props) => theme.secondaryBg};
     position: relative;
 
     .intro{
@@ -39,26 +35,28 @@ const Wrapper = styled.section`
             margin-left: 2em;
             width: 100%;
             height: 1px;
-            background: ${({theme}: Props) => theme.txtPrimary400};
+            background: ${({ theme }: Props) => theme.txtPrimary400};
         }
     }
 
     .worked__with{
         margin-top: 4em;
-        justify-content: start;
-        grid-template-columns: repeat(auto-fit, minMax(6em, 1fr));
+        display: grid;
+        justify-content: center;
+        gap: 2em;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 
         img{
-            width: 70%;
+            height: 60px !important;
             cursor: pointer;
+            object-fit: contain !important;
             margin-inline: auto;
 
             &:not(:first-child){
                 transition: .4s;
-                object-fit: contain;
-                filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale"); /* Firefox 3.5+ */
-                filter: gray; /* IE6-9 */
-                -webkit-filter: grayscale(100%); /* Chrome 19+ & Safari 6+ */
+                filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
+                filter: gray;
+                -webkit-filter: grayscale(100%);
 
                 &:hover{
                     filter: none;
@@ -107,14 +105,14 @@ const Wrapper = styled.section`
             // position: relative;
             z-index: 10 !important;
             transition: .4s;
-            background: ${({theme}: Props) => theme.expCard};
+            background: ${({ theme }: Props) => theme.expCard};
 
             &:hover{
-                background: ${({theme}: Props) => theme.expCardHover};
-                color: ${({theme}: Props) => theme.txtPrimary100};
+                background: ${({ theme }: Props) => theme.expCardHover};
+                color: ${({ theme }: Props) => theme.txtPrimary100};
                 
                 h3, a{
-                    color: ${({theme}: Props) => theme.txtPrimary100};
+                    color: ${({ theme }: Props) => theme.txtPrimary100};
                 }
                 p{
                     opacity: 1;
@@ -145,52 +143,52 @@ const Wrapper = styled.section`
     
 `;
 
-const Work= () => {
+const Work = () => {
     return (
         // <Fade ssrFadeout bottom>
-            <Wrapper>
-                <h1 className="side">Work</h1>
-                <Container>
-                    <p className="flex intro">Companies I&#39;ve Worked With <span className="line"></span></p>
-                    <div className="grid worked__with">
-                        <Image src={exai} alt="Examroom.AI" />
-                        <Image src={bict} alt="Bonitas ICT" />
-                        <Image src={nova} alt="Innovate Space" />
-                        <Image src={oakSoft} alt="The Oaksoft " />
+        <Wrapper>
+            <h1 className="side">Work</h1>
+            <Container>
+                <p className="flex intro">Companies I&#39;ve Worked With <span className="line"></span></p>
+                <div className="grid worked__with">
+                    <Image src={fm} alt="Featuremind" height={60} width={212} />
+                    <Image src={oyiche} alt="Oyiche Tech" height={60} width={212} />
+                    <Image src={groovinx} alt="Groovinx" height={60} width={212} />
+                    {/*    <Image src={oakSoft} alt="The Oaksoft " />
                         <Image src={cousant} alt="Cousant Connect" />
                         <Image src={hoh} alt="House of Hishighnex " />
-                        <Image src={devAmplify} alt="Devamplify" />
-                    </div>
+                        <Image src={devAmplify} alt="Devamplify" /> */}
+                </div>
 
-                    <div className="grid expo even-columns">
+                <div className="grid expo even-columns">
+                    <Fade ssrFadeout bottom>
+                        <div className="expo__years">
+                            <p className="yr flex">6 <span>+</span></p>
+                            <p>Years of Professional Experience.</p>
+                        </div>
+                    </Fade>
+                    <div className="expo__skill-set">
+                        <p>I&#39;ve worked in full capacity as a fullstack Web and Mobile App developer in every of the afore mentioned organization.</p>
                         <Fade ssrFadeout bottom>
-                            <div className="expo__years">
-                                <p className="yr flex">6 <span>+</span></p>
-                                <p>Years Experience Working.</p>
+                            <div className="skills even-columns grid">
+                                {
+                                    skills.map(s => <SkillWrap {...s} key={s.title} />)
+                                }
                             </div>
                         </Fade>
-                        <div className="expo__skill-set">
-                            <p>I&#39;ve worked in full capacity as a fullstack Web and Mobile App developer in every of the afore mentioned organization.</p>
-                            <Fade ssrFadeout bottom>
-                                <div className="skills even-columns grid">
-                                    {
-                                        skills.map(s => <SkillWrap {...s} key={s.title} />)
-                                    }
-                                </div>
-                            </Fade>
-                        </div>
                     </div>
-                </Container>
-            </Wrapper>
+                </div>
+            </Container>
+        </Wrapper>
         // </Fade>
     );
 }
 
-const SkillWrap = ({title, icon}: d) => {
+const SkillWrap = ({ title, icon }: d) => {
     return (
         <div className="box">
             <i className={icon}></i>
-            <h3 dangerouslySetInnerHTML={{__html: title}}></h3>
+            <h3 dangerouslySetInnerHTML={{ __html: title }}></h3>
             <a href="mailto:vinsdev185@gmail.com"><p>Hire Me <i className='bx bx-right-arrow-alt'></i></p></a>
         </div>
     )
